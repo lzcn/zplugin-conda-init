4d435ddfd10aa4ba2f90dba3115c8e7745b03313
Zhi Lu <zhilu@std.uestc.edu.cn>
2020-10-19 19:38:41 +0800

    Update conda-init.plugin.zsh
    change env

--- conda-init.plugin.zsh
+++ conda-init.plugin.zsh
@@ -1,17 +1,18 @@
 0="${${ZERO:-${0:#$ZSH_ARGZERO}}:-${(%):-%N}}"
 
-if [ -z ${CONDA_PREFIX+x} ]; then
-	echo "Set CONDA_PREFIX to YOUR_CONDA_ROOT_PATH";
+if [ -z ${Z_INIT_CONDA_PREFIX+x} ]; then
+	echo "Set Z_INIT_CONDA_PREFIX to YOUR_CONDA_ROOT_PATH";
 else
-	__conda_setup="$($CONDA_PREFIX/bin/conda 'shell.zsh' 'hook' 2>/dev/null)"
+	__conda_setup="$($Z_INIT_CONDA_PREFIX/bin/conda 'shell.zsh' 'hook' 2>/dev/null)"
 	if [ $? -eq 0 ]; then
 		eval "$__conda_setup"
 	else
-		if [ -f "$CONDA_PREFIX/etc/profile.d/conda.sh" ]; then
-			. "$CONDA_PREFIX/etc/profile.d/conda.sh"
+		if [ -f "$Z_INIT_CONDA_PREFIX/etc/profile.d/conda.sh" ]; then
+			. "$Z_INIT_CONDA_PREFIX/etc/profile.d/conda.sh"
 		else
-			export PATH="$CONDA_PREFIX/bin:$PATH"
+			export PATH="$Z_INIT_CONDA_PREFIX/bin:$PATH"
 		fi
 	fi
 	unset __conda_setup
+	unset Z_INIT_CONDA_PREFIX
 fi
